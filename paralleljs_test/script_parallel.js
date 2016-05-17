65669d4d5d95e1543a2863e45871c4eeb1695bc2
Sunghyo Chung <s94021@naver.com>
2016-05-18 03:38:34 +0900

    Test parallel.js

--- paralleljs_test/script_parallel.js
+++ paralleljs_test/script_parallel.js
@@ -1,49 +1,39 @@
 var valarr = [28, 18, 17, 8];
+var answer = [];
 
-var log = function () { 
-
-	// console.log(arguments); 
-	var toTime = new Date();
-
-	var differenceTravel = toTime.getTime() - fromTime.getTime();
-	var seconds = Math.floor((differenceTravel) / (1000));
-// document.write('+ seconds +');
-	console.log('log: '+seconds);
-	p.workerProcess.process.kill();
-
-	console.log(p);
-};
-
-function fib(n) {
-	return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
-};
+// function fib(n) {
+// 	return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
+// };
 
 function parallel() {
-	// console.log('pushed:'+p);
 
-	for(var i = 0; i<100; i++) {
-
+	// for(var i=0; i<10; i++) {
 	var fromTime = new Date();
+
 	var p = new Parallel(valarr);
-	// console.log(p);
+	var log = function () { 
 
-	// var p1 = new Parallel(valarr);
-	// var p2 = new Parallel(valarr);
+		// var toTime = new Date();
+		// var differenceTravel = toTime.getTime() - fromTime.getTime();
+		// var seconds = Math.floor((differenceTravel) / (1000));
+		// console.log('seconds: '+seconds);
 
-	// One gotcha: anonymous functions cannot be serialzed
-	// If you want to do recursion, make sure the function
-	// is named appropriately
+		console.log(arguments); 
+
+		console.log('a: '+answer);
+	};
+
+	function fib(n) {
+	// return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
+
+		// answer.push(n+1);
+		return n;
+	};
+	
 
 	p.map(fib).then(log);
-	// p1.map(fib).then(log);
-	// p2.map(fib).then(log);
+// }
 
-	// delete p;
-	// p = undefined;
-
-	// p.workerProcess.process.kill();
-	// Logs the first 7 Fibonnaci numbers, woot!
-	}
 }
 
 function single() {
