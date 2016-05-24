a3e90b1f99bbdca1ee69d493cebcec8859d96e5f
Sunghyo Chung <s94021@naver.com>
2016-05-25 02:55:20 +0900

    Separate deepVis api from images-demo.js

---
+++ convnetjs/demo/js/deepVis_api.js
@@ -0,0 +1,165 @@
+var get_layer_canvas = function(L, isFilter, grads, index, scale) {
+
+  var A = {};
+  if(isFilter) {
+    A = L.filters[index];
+  } else {
+    A = L.out_act;
+  }
+
+  var isColor = false;
+  if(A.depth == 3) { isColor = true; }
+  
+  var s = scale || 2; // scale
+  var draw_grads = false;
+  if(typeof(grads) !== 'undefined') draw_grads = grads;
+
+  // get max and min activation to scale the maps automatically
+  var w = draw_grads ? A.dw : A.w;
+  var mm = maxmin(w);
+
+  var canv = document.createElement('canvas');
+  canv.className = 'actmap';
+  var W = A.sx * s;
+  var H = A.sy * s;
+  canv.width = W;
+  canv.height = H;
+  var ctx = canv.getContext('2d');
+  var g = ctx.createImageData(W, H);
+
+  if(isColor) { // draw a color img
+    for(var d=0;d<3;d++) {
+      for(var x=0;x<A.sx;x++) {
+        for(var y=0;y<A.sy;y++) {
+          if(draw_grads) {
+            var dval = Math.floor((A.get_grad(x,y,d)-mm.minv)/mm.dv*255);
+          } else {
+            var dval = Math.floor((A.get(x,y,d)-mm.minv)/mm.dv*255);  
+          }
+          for(var dx=0;dx<s;dx++) {
+            for(var dy=0;dy<s;dy++) {
+              var pp = ((W * (y*s+dy)) + (dx + x*s)) * 4;
+              g.data[pp + d] = dval;
+              if(d===0) g.data[pp+3] = 255; // alpha channel
+            }
+          }
+        }
+      }
+    }
+  }
+  else { //draw a black & white img
+
+    if(isFilter) {
+      for(var x=0;x<A.sx;x++) {
+        for(var y=0;y<A.sy;y++) {
+          var dval = 0
+          //calculate average of the filter weights
+          for(var d=0;d<A.depth;d++) {
+            if(draw_grads) {
+              dval += Math.floor((A.get_grad(x,y,d)-mm.minv)/mm.dv*255);
+            } else {
+              dval += Math.floor((A.get(x,y,d)-mm.minv)/mm.dv*255);  
+            }
+          }
+          dval = dval/A.depth;
+          for(var dx=0;dx<s;dx++) {
+            for(var dy=0;dy<s;dy++) {
+              var pp = ((W * (y*s+dy)) + (dx + x*s)) * 4;
+              for(var i=0;i<3;i++) { g.data[pp + i] = dval; } // rgb
+              g.data[pp+3] = 255; // alpha channel
+            }
+          }
+        }
+      }
+    } else {
+
+      var d = index;
+
+      for(var x=0;x<A.sx;x++) {
+        for(var y=0;y<A.sy;y++) {
+          if(draw_grads) {
+            var dval = Math.floor((A.get_grad(x,y,d)-mm.minv)/mm.dv*255);
+          } else {
+            var dval = Math.floor((A.get(x,y,d)-mm.minv)/mm.dv*255);  
+          }
+          for(var dx=0;dx<s;dx++) {
+            for(var dy=0;dy<s;dy++) {
+              var pp = ((W * (y*s+dy)) + (dx + x*s)) * 4;
+              for(var i=0;i<3;i++) { g.data[pp + i] = dval; } // rgb
+              g.data[pp+3] = 255; // alpha channel
+            }
+          }
+        }
+      }
+    } //else end
+  } //else end
+  
+  ctx.putImageData(g, 0, 0);
+  return canv;
+}
+
+var get_grad_magnitude = function(L, isFilter, index){
+
+  var A = {};
+  if(isFilter) {
+    A = L.filters[index];
+  } else {
+    A = L.out_act;
+  }
+  var grad_magnitude = 0.0;
+
+  if(isFilter) {
+    for(var d=0;d<A.depth ; d++) {
+      for(var x=0;x<A.sx;x++) {
+        for(var y=0;y<A.sy;y++) {
+          var A_grad=A.get_grad(x,y,d);
+          grad_magnitude+=A_grad*A_grad;  
+        }
+      }
+    }
+  }else {
+    var d = index;
+    for(var x=0;x<A.sx;x++) {
+      for(var y=0;y<A.sy;y++) {
+        var A_grad=A.get_grad(x,y,d);
+        grad_magnitude+=A_grad*A_grad;  
+      }
+    }
+  }
+
+  return grad_magnitude;
+}
+
+var get_path_intensity = function(L, isFilter, index){
+
+  var A = {};
+  if(isFilter) {
+    A = L.filters[index];
+  } else {
+    A = L.out_act;
+  }
+  var path_intensity = 0.0;
+
+  if(isFilter) {
+    for(var d=0;d<A.depth ; d++) {
+      for(var x=0;x<A.sx;x++) {
+        for(var y=0;y<A.sy;y++) {
+          var act=A.get(x,y,d);
+          if(act < 0) act = 0;
+          path_intensity+=act;  
+        }
+      }
+    }
+  }else {
+    var d = index;
+    for(var x=0;x<A.sx;x++) {
+      for(var y=0;y<A.sy;y++) {
+        var act=A.get(x,y,d);
+        if(act < 0) act = 0;
+        path_intensity+=act;
+      }
+    }
+  }
+
+  return path_intensity;
+}
